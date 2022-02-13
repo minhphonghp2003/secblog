dba25fe02d6b9a1366ff5423a1fffff7f32fd055
minhphonghp2003 <minhphongph2003@gmail.com>
2022-02-13 05:00:12 -0800

    n

--- secblog/settings.py
+++ secblog/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True 
 
-ALLOWED_HOSTS = ['192.168.1.16','localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
