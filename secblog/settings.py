47829185219ed7c517a2df22c0b11325d02294b3
minhphonghp2003 <minhphongph2003@gmail.com>
2022-02-13 04:47:03 -0800

    n

--- secblog/settings.py
+++ secblog/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import json 
+
+with open('/etc/secblog_config.json') as config_file:
+    config = json.load(config_file)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u@bk2)5pnv)w%h&kvm8+*ovy1#3$_lj@3a_8z&y593o265ovmk'
+SECRET_KEY = config['SECRET_KEY'] 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.1.16','localhost']
 
 
 # Application definition
@@ -134,8 +139,9 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'nguyenminhhp2003@gmail.com'
-EMAIL_HOST_PASSWORD = 'minhphonGgoogle82'
+EMAIL_HOST_USER = config.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = config.get('EMAIL_PASS')
+
 
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
