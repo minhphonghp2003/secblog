45a22fbc90ba7fb5ed0e78096bd3dc02c8979481
minhphonghp2003 <minhphongph2003@gmail.com>
2022-02-17 21:24:04 -0800

    n

--- writeup/models.py
+++ writeup/models.py
@@ -18,7 +18,7 @@ class Cate(models.Model):
 class Writeup(models.Model):
     
     like = models.ManyToManyField(User,related_name='like')
-    title = models.CharField(max_length=100)
+    title = models.TextField()
     prerequis =  RichTextUploadingField(blank=True, null=True)
     content_upload = RichTextUploadingField(blank=True, null=True)
     cate = models.ManyToManyField(Cate, related_name='writeup')
